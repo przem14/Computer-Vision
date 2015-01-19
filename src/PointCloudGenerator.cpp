58bc7b02224d3fc309e8bd3b33a3b06fc1afd074
smalec <kaczy2406@gmail.com>
2015-01-19 10:56:14 +0100

    Issue #21: Added method to generate point cloud

--- src/PointCloudGenerator.cpp
+++ src/PointCloudGenerator.cpp
@@ -8,6 +8,12 @@ PointCloudGenerator::PointCloudGenerator(
     loadD2DMappingMatrix(pathToD2DMappingMatrix);
 }
 
+void PointCloudGenerator::generate() noexcept
+{
+    cv::reprojectImageTo3D(_disparityMap, _depthMap, _d2DMappingMatrix);
+    savePointsWithPlyExtension();
+}
+
 void PointCloudGenerator::loadDisparityMap(const std::string &pathToDisparityMap)
         noexcept
 {
@@ -22,4 +28,41 @@ void PointCloudGenerator::loadD2DMappingMatrix(
     cv::FileStorage fileStorage(pathToD2DMappingMatrix, cv::FileStorage::READ);
     fileStorage[D2D_MAPPING_MATRIX_TITLE] >> _d2DMappingMatrix;
     fileStorage.release();
+}
+
+void PointCloudGenerator::savePointsWithPlyExtension() noexcept
+{
+    int pointsAmount = computeOutput();
+    _outputFile.open(OUTPUT_FILENAME, std::ofstream::out);
+    addPlyHeader(pointsAmount);
+    _outputFile << _output.str();
+    _outputFile.close();
+    std::cout << "Point cloud saved to " << OUTPUT_FILENAME << std::endl;
+}
+
+void PointCloudGenerator::addPlyHeader(int pointsAmount) noexcept
+{
+    _outputFile << "ply" << std::endl;
+    _outputFile << "format ascii 1.0" << std::endl;
+    _outputFile << "element vertex " << pointsAmount << std::endl;
+    _outputFile << "property float32 x" << std::endl;
+    _outputFile << "property float32 y" << std::endl;
+    _outputFile << "property float32 z" << std::endl;
+    _outputFile << "end_header" << std::endl;
+}
+
+int PointCloudGenerator::computeOutput() noexcept
+{
+    int pointsAmount = 0;
+    for (int x = 0; x < _depthMap.rows; x++) {
+        for (int y = 0; y < _depthMap.cols; y++) {
+            cv::Point3f point = _depthMap.at<cv::Point3f>(x, y);
+            if(point.x > INFINITY_VALUE || point.x < -INFINITY_VALUE) continue;
+            if(point.y > INFINITY_VALUE || point.y < -INFINITY_VALUE) continue;
+            if(point.z > INFINITY_VALUE || point.z < -INFINITY_VALUE) continue;
+            _output << point.x << " " << point.y << " " << point.z << std::endl;
+            pointsAmount++;
+        }
+    }
+    return pointsAmount;
 }
--- src/PointCloudGenerator.h
+++ src/PointCloudGenerator.h
@@ -1,9 +1,12 @@
 #ifndef POINTCLOUDGENERATOR_H
 #define POINTCLOUDGENERATOR_H
 
+#include <opencv2/highgui/highgui.hpp>
 #include <opencv2/calib3d/calib3d.hpp>
 
 #include <string>
+#include <iostream>
+#include <fstream>
 
 class PointCloudGenerator
 {
@@ -11,12 +14,29 @@ public:
     PointCloudGenerator(const std::string& pathToDisparityMap,
                         const std::string& pathToD2DMappingMatrix) noexcept;
 
+    void generate() noexcept;
+
     void loadDisparityMap(const std::string& pathToDisparityMap) noexcept;
     void loadD2DMappingMatrix(const std::string& pathToD2DMappingMatrix) noexcept;
 
 private:
+    void savePointsWithPlyExtension() noexcept;
+
+    void addPlyHeader(int pointsAmount) noexcept;
+    int computeOutput() noexcept;
+
+
+
     cv::Mat _disparityMap;
     cv::Mat _d2DMappingMatrix;
+    cv::Mat _depthMap;
+
+    std::stringstream _output;
+    std::ofstream _outputFile;
+
+    const int INFINITY_VALUE = 500;
+
+    const std::string OUTPUT_FILENAME = "points.ply";
 
     const std::string DISPARITY_MAP_TITLE = "Disparity Map";
     const std::string D2D_MAPPING_MATRIX_TITLE =
